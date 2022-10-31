98c8df44af2dff61a2ec190879091ff3baa82ae7
georgecassens <georgecassens6@gmail.com>
2022-10-30 20:29:10 -0600

    Hw9

---
+++ Hw9/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(400,400);
+  }
+  
+  function draw() {
+    background(220);
+    stroke(200)
+    fill(150, 75, 0)
+    circle(200,200,100);
+    fill(0,0,0)
+    rect(160,150,30,20);
+    rect(200,150,30,20)
+    stroke(100)
+strokeWeight(15)
+    point(170, 190);
+    point(215, 190);
+    strokeWeight(1)
+    stroke(20)
+    line(200, 250, 200, 400);
+    fill(255, 0, 0)
+    ellipse(200, 222, 60, [30])
+    fill(0, 0, 255)
+    textSize(20);
+    triangle(170, 330, 200, 280, 225, 330);
+    fill(0, 0, 0)
+    text('Coded Shapes of George', 10, 30);
+    text('-George', 10, 380);
+   
+ 
+}
+
+
+  
