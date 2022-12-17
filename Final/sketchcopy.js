d9b9f62bb908c6d501de8b1a7e96bfa3ec9fcbc5
georgecassens <georgecassens6@gmail.com>
2022-12-16 18:49:05 -0700

    final

--- Final/sketchcopy.js
+++ Final/sketchcopy.js
@@ -21,19 +21,22 @@ createCanvas (500,600);
 }
 
 function draw(){
-    background(200,5,70);
+ preload()
+    imageMode(CENTER)
+ image(landscape, width/2, height/2, width, height);
+
     character()
     obstacles()
-    
-    
-     
+    charactermovement ()
+    youwin()
+     youlose()
      textSize(20);
     text("X: " + mouseX,100,200 );
     text("Y: " + mouseY,100,220 );
     for (var i = 0; i < shapeXs.length; i++) 
 
     circle(shapeXs[i], shapeYs[i], diameters[i]);
-    if(counter >=50 )   counter =0{
+    if(counter >=50 )   {
      
         counter++
     shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 1)) + 1);
@@ -45,7 +48,9 @@ function draw(){
         shapeYs[i] = getRandomNumber(600);
         diameters[i] = getRandomNumber(30);
     } 
+    counter =10
 }
+counter++
 }
 
 function character()
@@ -61,4 +66,53 @@ function obstacles()
 }
 function getRandomNumber(number) {
     return Math.floor(Math.random() * number) + 2;
+}
+function charactermovement()
+{
+    if(keyIsDown(w))
+    {
+        characterY -= 5;   
+    }
+    if(keyIsDown(s))
+    {
+        characterY += 5;   
+    }
+    if(keyIsDown(a))
+    {
+        characterX -= 5;   
+    }
+    if(keyIsDown(d))
+    {
+        characterX += 5;   
+    }
+
+}
+function youwin()
+{
+    if(characterX > 250&& characterX <300 && characterY > 275 && characterY < 325)
+    {
+        fill(0);
+        stroke(5);
+        textSize(26);
+        text("You Win!", width/2-50, height/2-50);
+    }
+}
+function youlose()
+{
+    if(characterX > 105 && characterX < 155 && characterY > 434 && characterY <486 )
+{
+    fill(255)
+    textSize(26);
+    text("You LOSE", width/2-50, height/2-50);
+}
+}
+if(characterX > 334 && characterX < 384 && characterY > 475 && characterY <524 )
+{
+    fill(255)
+    textSize(26);
+    text("You LOSE", width/2-50, height/2-50);
+}
+function preload()
+{
+landscape = loadImage('IMG_4861.jpg');
 }
